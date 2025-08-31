db8364f29eeea2dbe2651862c518cd0cfe2a7d5c
Nerio Ferrebus <nerio.ferrebuscastillo@unicolombo.edu.co>
2025-08-31 15:48:44 -0500

    avances login 3

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ValueAccessor } from '@ionic/angular/common';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class LoginPage implements OnInit {
+public email!: FormControl;
+public password!: FormControl;
+public loginForm!: FormGroup;
+public r!: FormBuilder;
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
+    this.initForm();
+
+   }
+  
+  ngOnInit() {}
+
+ public onLogin(){
+  console.log(this.loginForm.value)
+ }
+
+
+  private initForm(){
+     this.email = new FormControl('', [Validators.required, Validators.email]);
+         this.password = new FormControl('', [
+          Validators.required, 
+          Validators.minLength(6),
+        Validators.maxLength(12),
+          ]);
+          this.loginForm = new FormGroup({
+            email: this.email,
+           password: this.password,
+         } );
+          
+
   }
 
 }
--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { InputComponent } from './components/input/input.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [InputComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class SharedModule { }
